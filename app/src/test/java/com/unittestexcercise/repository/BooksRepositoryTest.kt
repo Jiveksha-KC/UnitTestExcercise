40654bbb8fce1a933eeea0e11694dc8a4913eebd
Kirsty Butler <kirsty.butler1@tesco.com>
2023-02-22 15:55:07 +0000

    inital commit

--- app/src/test/java/com/unittestexcercise/repository/BooksRepositoryTest.kt
+++ app/src/test/java/com/unittestexcercise/repository/BooksRepositoryTest.kt
@@ -1,4 +1,76 @@
 package com.unittestexcercise.repository
 
+import com.google.common.truth.Truth.assertThat
+import com.unittestexcercise.MockKRule
+import com.unittestexcercise.networkapi.BookApi
+import com.unittestexcercise.networkapi.model.BookResponse
+import com.unittestexcercise.stubs.BookResponseStub
+import io.mockk.coEvery
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 class BooksRepositoryTest {
+
+    private companion object {
+        val SOME_BOOK = BookResponseStub.new()
+        val SOME_BOOKS = listOf(SOME_BOOK)
+        val SOME_EMPTY_LIST: List<BookResponse> = emptyList()
+    }
+
+    @get: Rule
+    val mockkRule = MockKRule()
+
+    @MockK
+    private lateinit var mockBookApi: BookApi
+
+    @InjectMockKs
+    private lateinit var subject: BooksRepository
+
+    @Before
+    fun setup() {
+        // GIVEN
+        coEvery { mockBookApi.getBooks() } returns SOME_BOOKS
+    }
+
+    @Test
+    fun `getBooks() - THEN books is returned from book api`() {
+        // WHEN
+        val result = runBlocking { subject.getBooks() }
+
+        // THEN
+        assertThat(result).isEqualTo(SOME_BOOKS)
+    }
+
+    @Test
+    fun `getBooks() with empty list - THEN no books returned`() {
+        // GIVEN
+        coEvery { mockBookApi.getBooks() } returns SOME_EMPTY_LIST
+
+        // WHEN
+        val result = runBlocking { subject.getBooks() }
+
+        // THEN
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun `getBook() - THEN book returned from book api`() {
+        // GIVEN
+        coEvery { mockBookApi.getBook(SOME_BOOK.id) } returns SOME_BOOK
+
+        // WHEN
+        val result = runBlocking { subject.getBook(SOME_BOOK.id) }
+
+        // THEN
+        // access properties of the response
+//        with(SOME_BOOK) {
+//            assertThat(id).isEqualTo(id)
+//            assertThat(result).isEqualTo(this)
+//        }
+        assertThat(result).isEqualTo(SOME_BOOK)
+    }
 }
--- app/src/test/java/com/unittestexcercise/stubs/BookResponseStub.kt
+++ app/src/test/java/com/unittestexcercise/stubs/BookResponseStub.kt
@@ -1,4 +1,17 @@
 package com.unittestexcercise.stubs
 
-class BookResponseStub {
+import com.unittestexcercise.networkapi.model.BookResponse
+
+object BookResponseStub {
+    const val ID = 1
+    const val TITLE = "title"
+    const val CAPTION = "caption"
+    const val AUTHOR = "author"
+
+    fun new(id: Int = ID,
+            title: String = TITLE,
+            caption: String = CAPTION,
+            author: String = AUTHOR
+    ) = BookResponse(id, title, caption, author)
 }
+
