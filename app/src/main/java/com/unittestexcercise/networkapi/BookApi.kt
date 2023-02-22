da0e7f23f0853da2ebb17d123ca98f89e3c9a2d7
Jiveksha Bhosle <jiveksha.bhosle@kinandcarta.com>
2023-02-22 15:57:03 +0000

    Updated repository tests

--- app/src/main/java/com/unittestexcercise/networkapi/BookApi.kt
+++ app/src/main/java/com/unittestexcercise/networkapi/BookApi.kt
@@ -1,10 +1,13 @@
 package com.unittestexcercise.networkapi
 
 import com.unittestexcercise.networkapi.model.BookResponse
+import com.unittestexcercise.networkapi.model.UpdateAuthorRequest
 
 interface BookApi {
 
     suspend fun getBooks(): List<BookResponse>
 
     suspend fun getBook(id: Int): BookResponse
+
+    suspend fun updateBookAuthor(updateAuthorRequest: UpdateAuthorRequest)
 }
